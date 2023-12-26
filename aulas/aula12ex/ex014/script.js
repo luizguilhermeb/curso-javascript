86793bbe92f325b8db28447dc75efbcbdae31d46
luizguilhermeb <83599309+luizguilhermeb@users.noreply.github.com>
2023-12-26 19:09:56 -0300

    melhoria da lógica
    adicionei casas decimais ao horário, verificações de otimização

--- aulas/aula12ex/ex014/script.js
+++ aulas/aula12ex/ex014/script.js
@@ -1,8 +1,9 @@
 // Variáveis de hora declaradas e executadas
 let horaSistema = new Date().getHours()
 let minutosSistema = new Date().getMinutes()
+let horarioSistema = `${horaSistema}:${minutosSistema}`
 let horarioHTML = document.querySelector('#horario')
-horarioHTML.innerText = `${horaSistema}:${minutosSistema}`
+horarioHTML.innerText = `${String(horaSistema).length == 1 ? `0${horaSistema}` : horaSistema}:${String(minutosSistema).length == 1 ? `0${minutosSistema}` : minutosSistema}`
 
 // Imagem e frase complementar declaradas como variáveis e executadas
 let imgHora = document.querySelector('#img-hora')
@@ -10,34 +11,77 @@ let frasePositiva = document.querySelector('#frase-positiva')
 if (horaSistema < 6) {
     imgHora.src = 'img/madrugada.jpg'
     frasePositiva.innerText = 'Aí não né, vai dormir!'
+    document.body.style.backgroundImage = 'linear-gradient(to top, #000000, #1f1d1d)'
 } else if (horaSistema < 12) {
     imgHora.src = 'img/dia.jpg'
     frasePositiva.innerText = 'Tenha uma linda Manhã!'
+    document.body.style.backgroundImage = 'linear-gradient(to top, #eeff00, #ffc800)'
 } else if (horaSistema < 18) {
     imgHora.src = 'img/tarde.jpg'
     frasePositiva.innerText = 'Tenha uma bela Tarde!'
+    document.body.style.backgroundImage = 'linear-gradient(to top, #ffb300, #ff9100)'
 } else if (horaSistema < 24) {
     imgHora.src = 'img/noite.jpg'
     frasePositiva.innerText = 'Boa noite'
+    document.body.style.backgroundImage = 'linear-gradient(to top, #292626, #3c3838)'
 }
 
-// A cada X segundos se mudam as variáveis de acordo com o horário
-function mudarHorario() {
-    let horaSistema = new Date().getHours()
-    let minutosSistema = new Date().getMinutes()
-    horarioHTML.innerText = `${horaSistema}:${minutosSistema}`
+// Verificar se o minuto mudou, se mudou, executa as mudanças
+let periodoDia
+
+if (horaSistema < 6) {
+    periodoDia = 'madrugada'
+} else if (horaSistema < 12) {
+    periodoDia = 'manha'
+} else if (horaSistema < 18) {
+    periodoDia = 'tarde'
+} else if (horaSistema < 24) {
+    periodoDia = 'noite'
+}
+
+let periodoDiaFixo = periodoDia
+let horarioFixo = `${horaSistema}:${minutosSistema}`
+function verificarHorario() {
+    horaSistema = new Date().getHours()
+    minutosSistema = new Date().getMinutes()
+    horarioSistema = `${horaSistema}:${minutosSistema}`
+
     if (horaSistema < 6) {
-        imgHora.src = 'img/madrugada.jpg'
-        frasePositiva.innerText = 'Aí não né, vai dormir!'
+        periodoDia = 'madrugada'
     } else if (horaSistema < 12) {
-        imgHora.src = 'img/dia.jpg'
-        frasePositiva.innerText = 'Tenha uma linda Manhã!'
+        periodoDia = 'manha'
     } else if (horaSistema < 18) {
-        imgHora.src = 'img/tarde.jpg'
-        frasePositiva.innerText = 'Tenha uma bela Tarde!'
+        periodoDia = 'tarde'
     } else if (horaSistema < 24) {
-        imgHora.src = 'img/noite.jpg'
-        frasePositiva.innerText = 'Boa noite'
+        periodoDia = 'noite'
+    }
+    
+    if (horarioSistema != horarioFixo) {
+        horarioFixo = `${horaSistema}:${minutosSistema}`
+        horaSistema = new Date().getHours()
+        minutosSistema = new Date().getMinutes()
+        horarioHTML.innerText = `${String(horaSistema).length == 1 ? `0${horaSistema}` : horaSistema}:${String(minutosSistema).length == 1 ? `0${minutosSistema}` : minutosSistema}`
+        if (periodoDiaFixo != periodoDia) {
+            periodoDiaFixo = periodoDia
+            if (horaSistema < 6) {
+                imgHora.src = 'img/madrugada.jpg'
+                frasePositiva.innerText = 'Aí não né, vai dormir!'
+                document.body.style.backgroundImage = 'linear-gradient(to top, #000000, #1f1d1d)'
+            } else if (horaSistema < 12) {
+                imgHora.src = 'img/dia.jpg'
+                frasePositiva.innerText = 'Tenha uma linda Manhã!'
+                document.body.style.backgroundImage = 'linear-gradient(to top, #eeff00, #ffc800)'
+            } else if (horaSistema < 18) {
+                imgHora.src = 'img/tarde.jpg'
+                frasePositiva.innerText = 'Tenha uma bela Tarde!'
+                document.body.style.backgroundImage = 'linear-gradient(to top, #ffb300, #ff9100)'
+            } else if (horaSistema < 24) {
+                imgHora.src = 'img/noite.jpg'
+                frasePositiva.innerText = 'Boa noite'
+                document.body.style.backgroundImage = 'linear-gradient(to top, #292626, #3c3838)'
+            }
+        }
     }
 }
-setInterval(mudarHorario, 1000)
+
+setInterval(verificarHorario, 1000)
