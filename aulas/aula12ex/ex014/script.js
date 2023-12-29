36f21744928f0369532ee55bfab965402a516bca
luizguilhermeb <83599309+luizguilhermeb@users.noreply.github.com>
2023-12-29 20:30:55 -0300

    comentários do código atualizados

--- aulas/aula12ex/ex014/script.js
+++ aulas/aula12ex/ex014/script.js
@@ -1,11 +1,11 @@
-// Variáveis de hora declaradas e executadas
+// Receber a hora do sistema e injeta-la no elemento HTML
 let horaSistema = new Date().getHours()
 let minutosSistema = new Date().getMinutes()
 let horarioSistema = `${horaSistema}:${minutosSistema}`
 let horarioHTML = document.querySelector('#horario')
-horarioHTML.innerText = `${String(horaSistema).length == 1 ? `0${horaSistema}` : horaSistema}:${String(minutosSistema).length == 1 ? `0${minutosSistema}` : minutosSistema}`
+horarioHTML.innerText = `${String(horaSistema).length == 1 ? `0${horaSistema}` : horaSistema}:${String(minutosSistema).length == 1 ? `0${minutosSistema}` : minutosSistema}` // Se hora ou minuto for um número de 1 digito, é colocado um 0 ao lado. Ex: 6 = 06
 
-// Imagem e frase complementar declaradas como variáveis e executadas
+// Página é personalizada de acordo com o período do dia, através de elementos HTML
 let imgHora = document.querySelector('#img-hora')
 let frasePositiva = document.querySelector('#frase-positiva')
 if (horaSistema < 6) {
@@ -26,7 +26,7 @@ if (horaSistema < 6) {
     document.body.style.backgroundImage = 'linear-gradient(to top, #292626, #3c3838)'
 }
 
-// Verificar se o minuto mudou, se mudou, executa as mudanças
+// Recebe o periodo do dia
 let periodoDia
 
 if (horaSistema < 6) {
@@ -39,13 +39,17 @@ if (horaSistema < 6) {
     periodoDia = 'noite'
 }
 
+// Declara um periodo e horario fixo usado posteriormente na função verificarHorario() 
 let periodoDiaFixo = periodoDia
 let horarioFixo = `${horaSistema}:${minutosSistema}`
+
 function verificarHorario() {
+    // Atualiza as variáveis de hora do sistema
     horaSistema = new Date().getHours()
     minutosSistema = new Date().getMinutes()
     horarioSistema = `${horaSistema}:${minutosSistema}`
 
+    // Atualiza a variável de período do dia
     if (horaSistema < 6) {
         periodoDia = 'madrugada'
     } else if (horaSistema < 12) {
@@ -55,12 +59,14 @@ function verificarHorario() {
     } else if (horaSistema < 24) {
         periodoDia = 'noite'
     }
-    
+
+    // compara horário fixo com horário atualizado do sistema, se for um novo horário, muda o horário no HTML e atualiza o horário fixo
     if (horarioSistema != horarioFixo) {
         horarioFixo = `${horaSistema}:${minutosSistema}`
         horaSistema = new Date().getHours()
         minutosSistema = new Date().getMinutes()
         horarioHTML.innerText = `${String(horaSistema).length == 1 ? `0${horaSistema}` : horaSistema}:${String(minutosSistema).length == 1 ? `0${minutosSistema}` : minutosSistema}`
+        // Se o período do Dia atualizado for diferente do período do dia fixo, executa as mudanças no HTML
         if (periodoDiaFixo != periodoDia) {
             periodoDiaFixo = periodoDia
             if (horaSistema < 6) {
